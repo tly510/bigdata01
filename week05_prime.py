2cddbad45b6de390211b2d1abf8c5abd1563f9b4
tly510 <ehdgy510@gmail.com>
2025-04-03 12:31:41 +0900

    week05_prime new

--- week05_prime.py
+++ week05_prime.py
@@ -1,4 +1,4 @@
-def is_prime(number):
+def is_prime(number : int) -> bool:
     """
     Function to determine whether a number is a prime number
     :param number: The number to determine whether it is prime or not
@@ -13,8 +13,13 @@ def is_prime(number):
         return False
     return True
 
-n = int(input())
-if is_prime(n):
-    print(f"{n}는(은) 소수입니다.")
-else:
-    print(f"{n}는(은) 소수가 아닙니다!")
+
+n1 = int(input())
+n2 = int(input())
+
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for i in range(n1, n2+1):
+    if is_prime(i):
+        print(i, end=' ')
