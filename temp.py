b20396f235f183bb7eb20ee6520d42be265651aa
tly510 <ehdgy510@gmail.com>
2025-04-17 11:46:38 +0900

    v2.0  week07.py  update

---
+++ temp.py
@@ -0,0 +1,10 @@
+# split
+texts = "나의 사랑 나의 빛 호날두."
+texts_list = texts.split()
+# texts_list = texts.split('국')
+print(texts_list)
+
+#join
+subjects = ['파이썬', '자바', 'C언어']
+subjects_string = ''.join(subjects)
+print(subjects_string)
