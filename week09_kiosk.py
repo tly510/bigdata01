c3c344f457a2ef345fab9820bde98202f7549dad
tly510 <ehdgy510@gmail.com>
2025-04-18 11:26:12 +0900

    2.5 kiosk

---
+++ week09_kiosk.py
@@ -0,0 +1,19 @@
+# from kiosk import *
+from kiosk import drinks, display_menu, order_process, print_receipt
+
+while True:
+    try:
+        menu = int(input(display_menu()))
+        if len(drinks) >= menu >= 1:
+            order_process(menu - 1)
+        elif menu == len(drinks)+1:
+            print("주문을 종료합니다")
+            break
+        else:
+            print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
+    except ValueError:
+        print(f"문자를 입력할 수 없습니다. 숫자를 입력해주세요")
+
+print_receipt()
+# test()
+# print(total_price)
