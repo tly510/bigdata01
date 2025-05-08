cb85ca3a388554ac0ca200b5ec7464d96056e2b8
tly510 <ehdgy510@gmail.com>
2025-05-08 12:19:55 +0900

    v2.8 kiosk

--- temp.py
+++ temp.py
@@ -1,10 +1,5 @@
-# split
-texts = "나의 사랑 나의 빛 호날두."
-texts_list = texts.split()
-# texts_list = texts.split('국')
-print(texts_list)
+# file 10
+file = open("ticket.txt", "w")
+file.close()
 
-#join
-subjects = ['파이썬', '자바', 'C언어']
-subjects_string = ''.join(subjects)
-print(subjects_string)
+
